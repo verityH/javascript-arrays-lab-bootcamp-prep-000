36b4392022fe6fcb2cd8ce1a41c8f31b7f4f13ed
Verity Hill <verity.lowenna@gmail.com>
2018-12-23 16:43:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,3 +25,7 @@ function destructivelyRemoveFirstKitten(name) {
 function appendKitten(name) {
   return kittens.concat(name);
 }
+
+function prependKitten(name) {
+  return name.concat(kittens);
+}
